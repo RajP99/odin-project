9c74ade95afdf0b9817809b0fd7862739050772e
Raj Parikh <rajparikh90@gmail.com>
2021-06-05 23:50:15 -0400

    finished all exercises

--- javascript-exercises/sumAll/sumAll.js
+++ javascript-exercises/sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if ((x < 0) || (y < 0) || (typeof x != "number") || (typeof y != "number")){
+        return "ERROR";
+    }
+    
+    let solution = 0;
+    start = Math.min(x,y);
+    end = 1 + Math.max(x,y);
+
+    for (let i = start; i < end; i++){
+        solution += i;
+    }
+    return solution;
 
 };
 
--- javascript-exercises/tempConversion/tempConversion.js
+++ javascript-exercises/tempConversion/tempConversion.js
@@ -1,10 +1,24 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let x = (temp - 32) * 5/9;
+   if (Number.isInteger(x)) {
+    return x;
+   } else {
+     return helper(x);
+   }
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let x = (temp * (9/5) + 32);
+  if (Number.isInteger(x)) {
+    return x;
+   } else {
+     return helper(x);
+   }
 };
+
+function helper(temp){
+  return (Math.round(temp * 10) / 10);
+}
 
 module.exports = {
   ftoc,
