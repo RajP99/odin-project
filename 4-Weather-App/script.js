261a63a3c24fa9ee78d03782e7a5282c706aea9e
Raj Parikh <rajparikh90@gmail.com>
2021-06-22 22:42:43 -0400

    displaying relevant weather info

--- 4-Weather-App/script.js
+++ 4-Weather-App/script.js
@@ -12,13 +12,40 @@ form.addEventListener("submit", async function(e) {
     const city = document.getElementById("city-input");
     try {
         const info = await getWeather(city.value);
-        console.log(info["weather"][0]);
+        console.log(info);
 
-        const test = document.getElementById("test");
-        const title = document.createElement("h3");
-        title.textContent = info["weather"][0]["description"];
-        test.appendChild(title);
+        createCard(info);
+
     } catch (error) {
         alert("Please enter a valid City!");
     }
-});
+}); 
+
+function createCard(info) {
+        const card = document.getElementById("weather-card");
+
+        const city = document.createElement("h1");
+        city.textContent = info["name"];
+
+        const country = document.createElement("h1");
+        country.textContent = info["sys"]["country"];
+
+        const temperature = document.createElement("h1");
+        temperature.textContent = info["main"]["temp"];
+
+        const description = document.createElement("h1");
+        description.textContent = info["weather"][0]["description"];
+
+        const wind = document.createElement("h1");
+        wind.textContent = info["wind"]["speed"];
+        
+        const humidity = document.createElement("h1");
+        humidity.textContent = info["main"]["humidity"];
+
+        card.appendChild(city);
+        card.appendChild(country);
+        card.appendChild(temperature);
+        card.appendChild(description);
+        card.appendChild(wind);
+        card.appendChild(humidity);
+}
