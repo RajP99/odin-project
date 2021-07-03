1b4b3b3c5007a1d1746d5f6c5fd964509cd66eee
RajP99 <rajparikh90@gmail.com>
2021-07-02 20:56:08 -0400

    installed uniqid, finished assignment

--- React-Assignment/task-app/src/App.js
+++ React-Assignment/task-app/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from "react";
+import Overview from "./components/Overview";
+import uniqid from "uniqid";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      task: { text: ''},
+      task: { text: '',
+              id: uniqid()
+            },
       tasks: []
     }
   }
 
   getTaskText = (e) => {
     this.setState({
-      task: e.target.value,
+      task: {
+        text: e.target.value,
+        id: this.state.task.id
+      },
     })
   }
 
@@ -20,7 +27,10 @@ class App extends Component {
     e.preventDefault();
     this.setState({
       tasks: this.state.tasks.concat(this.state.task),
-      task: { text: '' },
+      task: { 
+        text: '',
+        id: uniqid()
+      },
     });
   }
 
@@ -34,6 +44,7 @@ class App extends Component {
           <input onChange={this.getTaskText} value={task.text} type="text" id="taskInput"></input>
           <button type="submit">Submit Task</button>
         </form>
+        <Overview tasks={tasks}></Overview>
       </div>
     );
   }
