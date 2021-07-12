cf62801554cd5d238711d6dc856eb3356d50c461
RajP99 <rajparikh90@gmail.com>
2021-07-11 22:18:27 -0400

    added check for empty cart

--- 7-shopping-cart/src/pages/Checkout.js
+++ 7-shopping-cart/src/pages/Checkout.js
@@ -6,42 +6,45 @@ const Checkout = ({ cart, addItemToCart, removeItemFromCart }) => {
 
     const changeQuantity = (e, item) => {
         if (e.target.value > item.quantity) {
-            addItemToCart(item)
+            addItemToCart(item);
         } else {
-            removeItemFromCart(item)
+            removeItemFromCart(item);
         }
     }
 
     return (
         <div>
-            <ul>
-                {cart.map((item) => {
-                    let img = images("./" + item.imgs[0]);
-                    return (
-                        <div className="item-container-checkout">
-                            <img className="image-checkout" src={img.default} alt={item.name}></img>
-                            <div className="item-details-checkout">
-                                <h1>{item.name}</h1>
-                                <h1>{item.price}</h1>
-                                <br></br><br></br><br></br>
-                                <button onClick={() => removeItemFromCart(item, "All")}>
-                                    Remove item
-                                </button>
-                                <div className="quantity">
-                                    <input type="number" id="quantity" name="quantity" min="1" value={item.quantity} 
-                                    onChange={(e) => changeQuantity(e, item)}></input>
+            {cart.length > 0 ?
+                (<ul>
+                    {cart.map((item) => {
+                        let img = images("./" + item.imgs[0]);
+                        return (
+                            <div className="item-container-checkout">
+                                <img className="image-checkout" src={img.default} alt={item.name}></img>
+                                <div className="item-details-checkout">
+                                    <h1>{item.name}</h1>
+                                    <h1>{item.price}</h1>
+                                    <br></br><br></br><br></br>
+                                    <button onClick={() => removeItemFromCart(item, "All")}>
+                                        Remove item
+                                    </button>
+                                    <div className="quantity">
+                                        <input type="number" id="quantity" name="quantity" min="1" value={item.quantity} 
+                                        onChange={(e) => changeQuantity(e, item)}></input>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    )})
-                }
+                        )})
+                    }
 
-                <p>
-                Total:{" "} {cart.reduce((total, item) => {
-                    return total + item.price * item.quantity;
-                    }, 0).toFixed(2)}
-                </p>
-            </ul>
+                    <p>
+                    Total:{" "} {cart.reduce((total, item) => {
+                        return total + item.price * item.quantity;
+                        }, 0).toFixed(2)}
+                    </p>
+                </ul>)
+            : (<h1>Your Cart is Empty</h1>)
+            }
         </div>
     )
 }
